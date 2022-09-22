60c76c8269cc5b88961d08e1716f3df10f584f96
james-clegg <j.clegg1@hotmail.co.uk>
2022-09-23 00:30:03 +0100

    Added first API calls

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-const updateOutagesWithDeviceName = () => {
+import { getAllOutages, getSiteInfo } from "./endpoints";
+
+const updateOutagesWithDeviceName = async (siteID: string) => {
   // Retrieve all outages from the GET endpoint
+  const outages = await getAllOutages();
 
   // Retrieve the site info for norwich-pear-tree from that GET endpoint
+  const siteInfo = await getSiteInfo(siteID);
 
   // Filter out old outages and those that aren't from the norwich-pear-tree site
 
@@ -10,9 +14,9 @@ const updateOutagesWithDeviceName = () => {
   // Send it to the POST endpoint to update the site info
 
   // Notes:
-  // Handle the 500 with a try catch 
+  // Handle the 500 with a try catch
   // Don't forget the api key
   // Base path is https://api.krakenflex.systems/interview-tests-mock-api/v1/
 };
 
-updateOutagesWithDeviceName();
+updateOutagesWithDeviceName("norwich-pear-tree");
