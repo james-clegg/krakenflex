2954d397c03a43e84b73b1350895e18a73267bfa
james-clegg <j.clegg1@hotmail.co.uk>
2022-09-22 23:35:19 +0100

    Added functions to make requests

---
+++ src/endpoints.ts
@@ -0,0 +1,32 @@
+import { IEnhancedOutage, IOutage, ISite } from "./types";
+
+const baseUrl = "https://api.krakenflex.systems/interview-tests-mock-api/v1";
+const headers = {
+  "x-api-key": process.env.API_KEY || "",
+};
+
+export const getAllOutages = async (): Promise<IOutage[]> => {
+  const response = await fetch(`${baseUrl}/outages`, {
+    method: "GET",
+    headers,
+  });
+  return response.json();
+};
+
+export const getSiteInfo = async (siteID: string): Promise<ISite> => {
+  const response = await fetch(`${baseUrl}/site-info/${siteID}`, {
+    method: "GET",
+    headers,
+  });
+  return response.json();
+};
+
+export const sendUpdatedOutages = async (
+  siteID: string,
+  outages: IEnhancedOutage[]
+): Promise<void> => {
+  await fetch(`${baseUrl}/site-outages/${siteID}`, {
+    method: "POST",
+    headers: { ...headers, "content-type": "application/json" },
+  });
+};
