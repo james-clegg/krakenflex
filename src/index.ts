81fa2292aa25b16bb36c38a46f24aa7c0e84bed8
james-clegg <j.clegg1@hotmail.co.uk>
2022-09-22 22:39:45 +0100

    Laid out comments for solution

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-const helloWorld = () => {
-  console.log("Hello world");
+const updateOutagesWithDeviceName = () => {
+  // Retrieve all outages from the GET endpoint
+
+  // Retrieve the site info for norwich-pear-tree from that GET endpoint
+
+  // Filter out old outages and those that aren't from the norwich-pear-tree site
+
+  // Attach the device display name to each outage
+
+  // Send it to the POST endpoint to update the site info
+
+  // Notes:
+  // Handle the 500 with a try catch 
+  // Don't forget the api key
+  // Base path is https://api.krakenflex.systems/interview-tests-mock-api/v1/
 };
 
-helloWorld();
+updateOutagesWithDeviceName();
