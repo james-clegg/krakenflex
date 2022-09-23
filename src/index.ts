8413aa0a5a20af543607b647d2abcc90e00191e6
james-clegg <j.clegg1@hotmail.co.uk>
2022-09-23 18:44:42 +0100

    Put program together inside index.js

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
-import { getAllOutages, getSiteInfo } from "./endpoints";
+import { getAllOutages, getSiteInfo, sendUpdatedOutages } from "./endpoints";
+import { filterOldOrIrrelevantOutages } from "./utils";
 
 const updateOutagesWithDeviceName = async (siteID: string) => {
-  // Retrieve all outages from the GET endpoint
-  const outages = await getAllOutages();
-
-  // Retrieve the site info for norwich-pear-tree from that GET endpoint
-  const siteInfo = await getSiteInfo(siteID);
-
-  // Filter out old outages and those that aren't from the norwich-pear-tree site
-
-  // Attach the device display name to each outage
-
-  // Send it to the POST endpoint to update the site info
-
-  // Notes:
-  // Handle the 500 with a try catch
-  // Don't forget the api key
-  // Base path is https://api.krakenflex.systems/interview-tests-mock-api/v1/
+  try {
+    const outages = await getAllOutages();
+    const siteInfo = await getSiteInfo(siteID);
+    const filteredOutages = filterOldOrIrrelevantOutages(outages, siteInfo);
+    const requestSucceeded = await sendUpdatedOutages(filteredOutages, siteID);
+    requestSucceeded && console.log('Outages successfully sent.')
+  } catch (e) {
+    console.error("Error::", e);
+  }
 };
 
 updateOutagesWithDeviceName("norwich-pear-tree");
